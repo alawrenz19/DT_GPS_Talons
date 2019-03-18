f6323c9cb45fd3b7d1c3bf6a6f9e2baee3ccc2e2
alawrenz19 <33064599+alawrenz19@users.noreply.github.com>
2019-03-17 20:51:55 -0500

    Update GPS_Servo.py

--- GPS_Servo.py
+++ GPS_Servo.py
@@ -1,7 +1,7 @@
 import RPL
 RPL.init()
 import time
-import GPS_2018
+from GPS_2018 import vec_angle.py
 
 f_reverse = 1000
 neutral = 1500
@@ -14,6 +14,10 @@ def DT_PWM_Establish():
 	ServoL = int(raw_input("Pls input what pin you've inserted your left talon into > "))
 	RPL.pinMode(ServoL, RPL.PWM)
 
+#if __name__ == "__main__": use later
 DT_PWM_Establish()
+
+
+
 
 # vec_angle(x1, y1, x2, y2) Brad: input the GPS coordinates into here. 
